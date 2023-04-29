7eae8fb24a7dd500f52a0ae3e889a87ce2647dbd
lebe24 <emmanuel.philipel@yahoo.com>
2023-04-29 08:48:13 +0100

    feat: coffe app

---
+++ coffee/lib/home.page.dart
@@ -0,0 +1,98 @@
+import 'package:coffee/config/colors_constants.dart';
+import 'package:coffee/config/services_locator.dart';
+import 'package:coffee/models/coffee_item.model.dart';
+import 'package:coffee/models/treat_item.model.dart';
+import 'package:coffee/services/navigation.service.dart';
+import 'package:coffee/widgets/checkout.widget.dart';
+import 'package:coffee/widgets/intro.widget.dart';
+import 'package:coffee/widgets/sweet_treats.widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+
+import 'coffe_details.page.dart';
+import 'widgets/coffee_list.widget.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // extendBodyBehindAppBar: true,
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: IconButton(
+            icon: const Icon(
+              FeatherIcons.chevronLeft,
+              color: Colors.black,
+              size: 30,
+            ),
+            onPressed: () {
+              locator.get<NavigationService>().navigatorKey.currentState!.pop();
+            },
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(
+                FeatherIcons.shoppingBag,
+                size: 30,
+                color: Colors.black,
+              ),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: Container(
+          clipBehavior: Clip.none,
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Navigator(
+                  key: locator.get<NavigationService>().navigatorKey,
+                  observers: [locator.get<NavigationService>().heroController],
+                  onGenerateRoute: (settings) {
+                    NavigationArguments? args = settings.arguments as NavigationArguments?;
+                    late Widget currentPage;
+                    bool toHome = false;
+
+                    if (args == null) {
+                      if (settings.name == 'home') {
+                        toHome = true;
+                      }
+                      currentPage = settings.name == 'home' ? const CofeeListWidget() : const IntroWidget();
+                    } else {
+                      currentPage = CoffeeDetailsPage(
+                        coffee: CoffeeItem.mockItems[args.coffee],
+                      );
+                      if (args.isSweetTreats) {
+                        currentPage = SweetTreatsWidget(coffee: CoffeeItem.mockItems[args.coffee]);
+                      }
+                      if (args.isCheckout) {
+                        currentPage = CheckoutWidget(
+                          coffee: CoffeeItem.mockItems[args.coffee],
+                          treat: args.treat != null ? TreatItem.mockItems[args.treat!] : null,
+                        );
+                      }
+                    }
+
+                    return PageRouteBuilder(
+                        transitionDuration: Duration(milliseconds: toHome ? 800 : 300),
+                        pageBuilder: (context, animation, secondaryAnimation) {
+                          return FadeTransition(
+                            opacity: animation,
+                            child: Container(color: Colors.white, child: currentPage),
+                          );
+                        });
+                  }),
+            ],
+          ),
+        ));
+  }
+}
